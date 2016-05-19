7eb67b2099eca1a784fae5ba0e6921cc044de038
lubien <lubien1996@gmail.com>
2016-05-19 16:06:23 -0300

    Setup webpack HMR

--- client/webpack.development.js
+++ client/webpack.development.js
@@ -1,7 +1,10 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     path.join(__dirname, './entry.js'),
   ],
   module: {
@@ -18,7 +21,11 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../public/scripts/'),
     filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
