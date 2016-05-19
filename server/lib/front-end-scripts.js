a0da5e367e9ccb654558366aba8505f2a604573e
lubien <lubien1996@gmail.com>
2016-05-19 16:06:51 -0300

    No need for this script

--- server/lib/front-end-scripts.js
+++ server/lib/front-end-scripts.js
@@ -4,7 +4,6 @@ export default function bundleScriptsPath() {
   // TODO: connect webpack bundle.js port to the config file
   return config.is.dev ? [
     'http://localhost:8080/bundle.js',
-    'http://localhost:35729/livereload.js',
   ] : [
     '/public/scripts/bundle.js',
   ];
