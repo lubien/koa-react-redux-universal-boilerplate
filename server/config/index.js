67550e1fe08a3dc09fa5c87a9f482bb77407417a
lubien <lubien1996@gmail.com>
2016-05-19 15:32:17 -0300

    Add BASE_URL to config

--- server/config/index.js
+++ server/config/index.js
@@ -6,11 +6,12 @@ const {
   PORT = 3000,
   HOST = 'localhost',
   SESSIONID = 'koa:sess',
+  BASE_URL = `http://${HOST}:${PORT}`,
   GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET,
 } = process.env;
 
 const config = {
-  ENV, HOST, PORT, SESSIONID, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET,
+  ENV, HOST, PORT, SESSIONID, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, BASE_URL,
   is: {
     dev: ENV === 'development',
     prod: ENV === 'production',
@@ -21,7 +22,7 @@ const specific = {};
 
 specific.development = {
   MONGO_URL: 'mongodb://localhost:27017/koa-react-boilerplate',
-  GITHUB_CALLBACK_URL: `http://${HOST}:${PORT}/auth/github/callback`,
+  GITHUB_CALLBACK_URL: `${BASE_URL}/auth/github/callback`,
 };
 specific.production = {};
 
