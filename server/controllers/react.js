5266a4467e46e752a16bc64f15d0243935079651
lubien <lubien1996@gmail.com>
2016-05-20 21:15:22 -0300

    Now we receive the user directly from the server :)

--- server/controllers/react.js
+++ server/controllers/react.js
@@ -2,7 +2,7 @@ import serverSideRender from '../lib/server-side-render';
 import frontEndScripts from '../lib/front-end-scripts';
 
 exports.index = async function reactAction(ctx) {
-  const { rendered, head } = await serverSideRender(ctx.request.originalUrl);
+  const { rendered, head } = await serverSideRender(ctx.request.originalUrl, ctx.state.user);
   const scripts = frontEndScripts();
 
   await ctx.render('index', {
--- server/lib/server-side-render.js
+++ server/lib/server-side-render.js
@@ -6,12 +6,17 @@ import { Provider } from 'react-redux';
 import store from '../../client/store';
 import routes from '../../client/routes';
 
-export default function serverSideRender(url) {
+export default function serverSideRender(url, user = { loggedIn: false }) {
   return new Promise((fulfill, reject) => {
     match({
       routes, location: url,
     }, (err, redirectLocation, renderProps) => {
       if (err) reject(err);
+
+      store.dispatch({
+        type: 'SET_LOGGED_IN_USER',
+        user,
+      });
 
       const rendered = ReactDOM.renderToString(
         <Provider store={store}>
