f78a9b2fc70398262d621755a832c6ac9c1d0dff
lubien <lubien1996@gmail.com>
2016-05-28 23:28:48 -0300

    Add devToolsExtension

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import user from './reducers/user';
@@ -12,13 +12,22 @@ const reducers = combineReducers({
 let initialState = {};
 
 const middleWares = [thunkMiddleware];
+const composables = [
+  applyMiddleware(...middleWares),
+];
 
 if (typeof window !== 'undefined' && window.__STORE__) { // eslint-disable-line
   initialState = JSON.parse(window.__STORE__); // eslint-disable-line
+
+  if (process && process.env && process.env.NODE_ENV !== 'production') {
+    // zalmoxisus/redux-devtools-extension
+    // https://github.com/zalmoxisus/redux-devtools-extension#implementation
+    composables.push(
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    );
+  }
 }
 
-const store = createStore(reducers, initialState,
-  applyMiddleware(...middleWares)
-);
+const store = createStore(reducers, initialState, compose(...composables));
 
 export default store;
