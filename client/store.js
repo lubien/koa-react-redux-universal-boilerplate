f580cd57fab0fcde1ce811abf87e59f6d3f3b498
lubien <lubien1996@gmail.com>
2016-05-28 23:18:36 -0300

    Organize store.js

--- client/store.js
+++ client/store.js
@@ -4,18 +4,21 @@ import thunkMiddleware from 'redux-thunk';
 import user from './reducers/user';
 import { routerReducer as routing } from 'react-router-redux';
 
+const reducers = combineReducers({
+  user,
+  routing,
+});
+
 let initialState = {};
+
+const middleWares = [thunkMiddleware];
 
 if (typeof window !== 'undefined' && window.__STORE__) { // eslint-disable-line
   initialState = JSON.parse(window.__STORE__); // eslint-disable-line
 }
 
-const store = createStore(combineReducers({
-  user,
-  routing,
-}),
-  initialState,
-  applyMiddleware(thunkMiddleware)
+const store = createStore(reducers, initialState,
+  applyMiddleware(...middleWares)
 );
 
 export default store;
