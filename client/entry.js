5ec00f8c03a1ff771491b0ce2850945e774a0074
lubien <lubien1996@gmail.com>
2016-05-30 23:22:44 -0300

    Client entry doesn't need to match anything

--- client/entry.js
+++ client/entry.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, match, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes/index';
 
 if (!module || module && !module.parent) {
-  const history = syncHistoryWithStore(browserHistory, store);
-
-  match({
-    routes, history,
-  }, (err, redirectLocation, renderProps) => {
-    if (err) throw err;
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router {...renderProps} />
-      </Provider>,
-      document.getElementById('app')
-    );
-  });
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router history={browserHistory} routes={routes} />
+    </Provider>,
+    document.getElementById('app')
+  );
 }
